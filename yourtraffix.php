293b04d7a33e30193278cc17a0293d16f71e6f23
Gilad takoni <gilad1987@gmail.com>
2020-05-17 19:53:49 +0300

    Commit release 0.0.7

--- yourtraffix.php
+++ yourtraffix.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Yourtraffix
  * Plugin URI:        https://wordpress.org/plugins/yourtraffix
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           0.0.6
+ * Version:           0.0.7
  * Author:            Yourtraffix
  * Author URI:        http://yourtraffix.com
  * License:           GPL-2.0+
