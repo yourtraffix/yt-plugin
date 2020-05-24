eac9c2a56e911a0406ab893743ce526819c9c26b
Gilad takoni <gilad1987@gmail.com>
2020-05-25 00:48:33 +0300

    Commit release 0.1.14

--- yourtraffix.php
+++ yourtraffix.php
@@ -36,6 +36,7 @@ if (!defined('WPINC')) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define('YOURTRAFFIX_VERSION', '1.0.0');
+define('PLUGIN_DIR',  plugins_url('', (__FILE__)));
 
 /**
  * The code that runs during plugin activation.
