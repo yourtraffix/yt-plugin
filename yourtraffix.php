4e0281c2a2885004d786a8c37656c72f7fef9f30
Gilad takoni <gilad1987@gmail.com>
2020-06-08 09:31:40 +0300

    Commit release 0.0.31

--- yourtraffix.php
+++ yourtraffix.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Yourtraffix
  * Plugin URI:        http://yourtraffix.com
  * Description:       YourTraffix Social Advertising Network enables your posts to appear on others websites.
- * Version:           0.0.30
+ * Version:           0.0.31
  * Author:            Yourtraffix
  * Author URI:        http://yourtraffix.com
  * License:           GPL-2.0+
